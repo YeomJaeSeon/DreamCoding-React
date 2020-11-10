21117fda78adbe0f27ec108a1bc414af7f66fa12
YeomJaeSeon <a89541457@gmail.com>
2020-11-10 14:31:43 +0900

    react template + 리액트 구조 + index.js파일의 react-dom공부

---
+++ react-basic/habit-tracker/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Second from './components/Second'
+
+function App() {
+  return (
+  <>
+    <h1>Hello!!! :)</h1>
+    <p>안녕 ㅠㅠ</p>
+    <Second />
+  </>
+  );
+  }
+export default App;
