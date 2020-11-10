f665dac1871c381b144a37a4ab4415803087f1a1
YeomJaeSeon <a89541457@gmail.com>
2020-11-10 15:17:22 +0900

    react-dom

--- react-basic/habit-tracker/src/App.jsx
+++ react-basic/habit-tracker/src/App.jsx
@@ -3,11 +3,11 @@ import Second from './components/Second'
 
 function App() {
   return (
-  <>
-    <h1>Hello!!! :)</h1>
-    <p>안녕 ㅠㅠ</p>
-    <Second />
-  </>
-  );
+      <>
+        <h1>Hello!!! :)</h1>
+        <p>안녕 ㅠㅠ</p>
+        <Second />
+      </>
+    );
   }
 export default App;
