8d56d32a716cb232758442920df5d0760efcf0d0
YeomJaeSeon <a89541457@gmail.com>
2020-11-22 20:03:27 +0900

    styling - flex box를 이용함

--- react-basic/youtube-ellie/src/components/Video/Video.jsx
+++ react-basic/youtube-ellie/src/components/Video/Video.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import styles from './Video.module.css';
 
-const Video = ({ item }) => {
-  const title = item.snippet.title;
-  return <h1>{title}</h1>;
+const Video = ({ item: { snippet } }) => {
+  //destrcuturing
+  const title = snippet.title;
+  const channelTitle = snippet.channelTitle;
+  const thumbnail = snippet.thumbnails.medium.url;
+
+  return (
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img src={thumbnail} alt="thumbnail" className={styles.thumbnail} />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.channelTitle}>{channelTitle}</p>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default Video;
--- react-basic/youtube-ellie/src/components/VideoList/Videos.jsx
+++ react-basic/youtube-ellie/src/components/VideoList/Videos.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Video from '../Video/Video';
+import styles from './Videos.module.css';
 
 const Videos = ({ items }) => {
   return (
-    <ul>
+    <ul className={styles.videos}>
       {items.map((item) => {
         return <Video key={item.id} item={item}></Video>;
       })}
