284a5b1cf7ec10977b101af2a43932dcc101aba1
YeomJaeSeon <a89541457@gmail.com>
2020-11-24 16:28:31 +0900

    search

--- react-basic/youtube-ellie/src/App.jsx
+++ react-basic/youtube-ellie/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Videos from './components/VideoList/Videos';
+import Header from './components/Header/Header';
+import styles from './App.module.css';
 
 const APIKEY = process.env.REACT_APP_API_KEY;
 
@@ -24,7 +26,34 @@ const App = () => {
       .catch((error) => console.log('error', error));
   }, []);
 
-  return <Videos items={items}></Videos>;
+  const handleSubmit = (search) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=16&q=${search}&key=${APIKEY}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        const searchResult = result.items
+          .filter((item) => item.id.kind !== 'youtube#channel')
+          .map((item) => {
+            return { ...item, id: item.id.videoId };
+          });
+        setItems(searchResult);
+      })
+      .catch((error) => console.log('error', error));
+  };
+
+  return (
+    <div className={styles.app}>
+      <Header onHandleSubmit={handleSubmit}></Header>
+      <Videos items={items}></Videos>
+    </div>
+  );
 };
 
 export default App;
