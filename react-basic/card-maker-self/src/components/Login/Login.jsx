91a1c7c2493fe541b7bdb7043c9790690a87b959
YeomJaeSeon <a89541457@gmail.com>
2021-01-19 19:52:07 +0900

    로그인까지

--- react-basic/card-maker-self/src/components/Login/Login.jsx
+++ react-basic/card-maker-self/src/components/Login/Login.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
 import styles from './Login.module.css';
+import {useHistory} from 'react-router-dom'
+import {firebaseInstance, authService} from '../../fbase'
 
 const Login = () => {
-  const login = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-      })
-      .catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-  };
+  const history = useHistory()
+  const login = (e)=>{
+    let provider;
+    if(e.target.id === 'google') provider = new firebaseInstance.auth.GoogleAuthProvider();
+    else if(e.target.id === 'github') provider = new firebaseInstance.auth.GithubAuthProvider();
+    // 깃허브는 아직 적용 x
+    authService
+    .signInWithPopup(provider)
+    .then((result) => {
+      history.push('/app')
+    }).catch((error) => {
+      alert("실패")
+    });
+  }
+
   return (
     <section className={styles.container}>
       <header className={styles.header}>
@@ -33,10 +27,10 @@ const Login = () => {
       </header>
       <div className={styles.loginBody}>
         <h1 className={styles.loginTitle}>Login</h1>
-        <button className={styles.google} onClick={login}>
+        <button className={styles.google} id='google' onClick={login}>
           Google
         </button>
-        <button className={styles.github} onClick={login}>
+        <button className={styles.github} id='github' onClick={login}>
           Github
         </button>
       </div>
